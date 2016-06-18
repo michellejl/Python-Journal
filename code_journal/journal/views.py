ef9fec03de7856bdda628985ce44050fff7fd0e0
Michelle Levine <michellejl@live.com>
2016-06-18 13:16:07 -0700

    starting to pull information dynamically into the view template thing

--- code_journal/journal/views.py
+++ code_journal/journal/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Entry
 
 # Create your views here.
 
+
 def entry_list(request):
-    return render(request, 'journal/entry_list.html', {})
+    entries = Entry.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
+    return render(request, 'journal/entry_list.html', {'entries':entries})
